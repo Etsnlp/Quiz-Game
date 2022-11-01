dc6bd2aa6bbf2c73d753d23fa9051494f7a4f038
Emir Sonalp <etsnlp@gmail.com>
2022-11-01 19:49:31 +0300

    v0.02
    +ScriptableObjects
    +Questions
    +Button Events etc.

---
+++ Quiz Game/Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Quiz Question", fileName = "New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [TextArea(2,4)]
+    [SerializeField] string question = "What is the best selling videogame of all time?";  
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
+
+    public string GetQuestion()
+    {
+        return question;
+
+    }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+
+    }
+
+
+
+
+}
+
+
